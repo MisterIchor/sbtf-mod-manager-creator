630881fdf177f54abddc46acffbf99f6ece215df
MisterIchor <superluigibrosforever@gmail.com>
2025-05-28 16:14:56 -0400

    Connected each tool section to create a mod to handle data changes.

--- src/2_create_mods/create_a_mod.gd
+++ src/2_create_mods/create_a_mod.gd
@@ -1,59 +1,71 @@
 extends Window
 
-enum ModType {NONE_SELECTED = -1, ACTION_THEME, GRAPHICS_REPLACER, DATA_CHANGER}
+enum ModeToolSelection {NONE_SELECTED, ACTION_THEME, GRAPHICS_REPLACER, DATA_CHANGER}
 
 var mod_name: String = ""
-var type: ModType = ModType.NONE_SELECTED
+var type: ModeToolSelection = ModeToolSelection.NONE_SELECTED
 var description: String = ""
 var data: Dictionary = {}
 
 @onready var mod_name_line_edit: LineEdit = $VBoxContainer/HBoxContainer/ModNameLineEdit
-@onready var mod_type_option_button: OptionButton = $VBoxContainer/HBoxContainer/ModTypeOptionButton
+@onready var mod_tool_selection: OptionButton = $VBoxContainer/HBoxContainer/ModToolSelection
 @onready var action_theme: HBoxContainer = $VBoxContainer/ToolsContainer/ActionTheme
 @onready var none_selected_label: Label = $VBoxContainer/ToolsContainer/NoneSelectedLabel
 @onready var tools_container: MarginContainer = $VBoxContainer/ToolsContainer
+@onready var graphic_replacer: HSplitContainer = $VBoxContainer/ToolsContainer/GraphicReplacer
+@onready var data_change: HBoxContainer = $VBoxContainer/ToolsContainer/DataChange
 
 
 
 func _ready() -> void:
-	if not Engine.is_editor_hint():
-		if not SBTFTool.verify_nwf() == 0:
-			var prompt: Window = Prompt.nwf_path.instantiate()
-			prompt.popup_exclusive_centered(self)
+	if not SBTFTool.verify_nwf() == 0:
+		var prompt: Window = Prompt.nwf_path.instantiate()
+		prompt.popup_exclusive_centered(self)
+		
+		while true:
+			await prompt.close_requested
 			
-			while true:
-				await prompt.close_requested
-				
-				if not Config.path_to_nwf.is_empty():
-					if not SBTFTool.verify_nwf() == 0:
-						break
-				
-				prompt = Prompt.nwf_path.instantiate()
-				prompt.popup_exclusive(self)
+			if not Config.path_to_nwf.is_empty():
+				if not SBTFTool.verify_nwf() == 0:
+					break
+			
+			prompt = Prompt.nwf_path.instantiate()
+			prompt.popup_exclusive(self)
 	
 	mod_name_line_edit.text_submitted.connect(_on_ModNameLineEdit_text_submitted)
-	mod_type_option_button.item_selected.connect(_on_ModTypeOptionButton_item_selected)
+	mod_tool_selection.item_selected.connect(_on_ModToolSelection_item_selected)
+	action_theme.data_changed.connect(_on_data_changed.bind("theme"))
+	graphic_replacer.data_changed.connect(_on_data_changed.bind("graphic"))
+	data_change.data_changed.connect(_on_data_changed.bind("data"))
+	
 
+
+
+func compile_mod() -> void:
+	return
+
+
+
+func _on_data_changed(changed_data: Dictionary, from: String) -> void:
+	data[from] = changed_data
 
 
 func _on_ModNameLineEdit_text_submitted(new_text: String) -> void:
 	mod_name = new_text
 
 
-func _on_ModTypeOptionButton_item_selected(new_option: int) -> void:
+func _on_ModToolSelection_item_selected(new_option: int) -> void:
 	type = new_option
 	
 	for i in tools_container.get_children():
 		i.hide()
 	
 	match type:
-		ModType.NONE_SELECTED:
+		ModeToolSelection.NONE_SELECTED:
 			none_selected_label.show()
-		ModType.ACTION_THEME:
+		ModeToolSelection.ACTION_THEME:
 			action_theme.show()
-			data.layer = Array().resize(8)
-			data.overwrites_existing_theme = false
-		ModType.GRAPHICS_REPLACER:
-			return
-		ModType.DATA_CHANGER:
-			return
+		ModeToolSelection.GRAPHICS_REPLACER:
+			graphic_replacer.show()
+		ModeToolSelection.DATA_CHANGER:
+			data_change.show()
