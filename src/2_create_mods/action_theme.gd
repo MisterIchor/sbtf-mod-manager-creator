c53ba8850f0d8bcb49cf483c4906986335f639cf
MisterIchor <superluigibrosforever@gmail.com>
2025-05-28 18:08:01 -0400

    Reworked layout of action theme section. Zeroed volume of all streams within the test player to prevent music from playing when it shouldn't. Data now includes whether the selected theme overwrites an existing theme.

--- src/2_create_mods/action_theme.gd
+++ src/2_create_mods/action_theme.gd
@@ -1,4 +1,4 @@
-extends HBoxContainer
+extends VBoxContainer
 
 signal data_changed(new_data: Dictionary)
 
@@ -6,24 +6,26 @@ var adrenaline: float = 0.0
 var _data: Dictionary = {}
 var _selected_theme: String = ""
 
-@onready var theme_name_line_edit: LineEdit = $SettingsContainer/AddThemeContainer/ThemeNameLineEdit
-@onready var selected_theme_option_button: OptionButton = $SettingsContainer/HBoxContainer/SelectedThemeOptionButton
-@onready var remove_theme_button: Button = $SettingsContainer/HBoxContainer/RemoveThemeButton
-@onready var add_theme_button: Button = $SettingsContainer/AddThemeContainer/AddThemeButton
+@onready var theme_settings_container: HBoxContainer = $ThemeSettingsContainer
+@onready var selected_theme_option_button: OptionButton = $GlobalThemeSettingsContainer/SelectedThemeOptionButton
+@onready var remove_theme_button: Button = $GlobalThemeSettingsContainer/RemoveThemeButton
+@onready var theme_name_line_edit: LineEdit = $GlobalThemeSettingsContainer/ThemeNameLineEdit
+@onready var add_theme_button: Button = $GlobalThemeSettingsContainer/AddThemeButton
+@onready var overwrite_theme_toggle: ToggleOptionInteractable = $ThemeSettingsContainer/LayerSettings/OverwriteThemeToggle
+@onready var test_theme_toggle: ToggleOptionInteractable = $ThemeSettingsContainer/TestContainer/TestThemeToggle
+@onready var space_beast_spawned_slider: SliderInteractable = $ThemeSettingsContainer/TestContainer/SpaceBeastSpawnedSlider
+@onready var beast_distance_to_player_slider: SliderInteractable = $ThemeSettingsContainer/TestContainer/BeastDistanceToPlayerSlider
 @onready var test_music_player: AudioStreamPlayer = $TestMusicPlayer
 @onready var test_music_player_stream: AudioStreamSynchronized = $TestMusicPlayer.stream
-@onready var test_theme_toggle: ToggleOptionInteractable = $TestContainer/TestThemeToggle
-@onready var space_beast_spawned_slider: SliderInteractable = $TestContainer/SpaceBeastSpawnedSlider
-@onready var beast_distance_to_player_slider: SliderInteractable = $TestContainer/BeastDistanceToPlayerSlider
 @onready var layers: Array[PathToInteractable] = [
-	$SettingsContainer/Layer0, 
-	$SettingsContainer/Layer1, 
-	$SettingsContainer/Layer2, 
-	$SettingsContainer/Layer3, 
-	$SettingsContainer/Layer4, 
-	$SettingsContainer/Layer5, 
-	$SettingsContainer/Layer6, 
-	$SettingsContainer/Layer7
+	$ThemeSettingsContainer/LayerSettings/Layer0, 
+	$ThemeSettingsContainer/LayerSettings/Layer1, 
+	$ThemeSettingsContainer/LayerSettings/Layer2,
+	$ThemeSettingsContainer/LayerSettings/Layer3,
+	$ThemeSettingsContainer/LayerSettings/Layer4,
+	$ThemeSettingsContainer/LayerSettings/Layer5,
+	$ThemeSettingsContainer/LayerSettings/Layer6,
+	$ThemeSettingsContainer/LayerSettings/Layer7
 ]
 
 
@@ -39,6 +41,7 @@ func _ready() -> void:
 	theme_name_line_edit.text_changed.connect(_on_ThemeNameLineEdit_text_changed)
 	theme_name_line_edit.text_submitted.connect(_on_AddThemeButton_pressed.unbind(1))
 	remove_theme_button.pressed.connect(_on_RemoveThemeButton_pressed)
+	overwrite_theme_toggle.toggled.connect(_on_OverwriteThemeToggle_toggled)
 
 
 
@@ -66,9 +69,9 @@ func _update_adrenaline() -> void:
 		test_music_player_stream.set_sync_stream_volume(i, linear_to_db(layer_volume[i]))
 
 
-func _update_audio_files() -> void:
-	for i in _data[_selected_theme].size():
-		var path: String = _data[_selected_theme][i]
+func _update_settings() -> void:
+	for i in _data[_selected_theme].layers.size():
+		var path: String = _data[_selected_theme].layers[i]
 		
 		if path.is_empty():
 			continue
@@ -79,13 +82,16 @@ func _update_audio_files() -> void:
 		audio_stream.loop_mode = AudioStreamWAV.LOOP_FORWARD
 		audio_stream.loop_end = audio_stream.mix_rate * audio_stream.get_length()
 		test_music_player_stream.set_sync_stream(i, audio_stream)
+		layers[i].line_edit.text = path
+	
+	overwrite_theme_toggle.check_button.button_pressed = _data[_selected_theme].overwrites_existing_theme
 
 
 
 func _on_Layer_path_set(path: String, layer_idx: int) -> void:
-	_data[_selected_theme][layer_idx] = path
+	_data[_selected_theme].layers[layer_idx] = path
 	data_changed.emit(_data)
-	_update_audio_files()
+	_update_settings()
 	
 	if test_music_player.playing:
 		test_music_player.play()
@@ -103,15 +109,20 @@ func _on_AddThemeButton_pressed() -> void:
 	if not _data.get(theme_name_line_edit.text):
 		var array: PackedStringArray = []
 		array.resize(8)
-		_data[theme_name_line_edit.text] = array
+		_data[theme_name_line_edit.text] = {
+			layers = array,
+			overwrites_existing_theme = false
+		}
 		data_changed.emit(_data)
 	
 	selected_theme_option_button.add_item(theme_name_line_edit.text)
 	selected_theme_option_button.select(selected_theme_option_button.item_count - 1)
+	theme_settings_container.show()
 	_selected_theme = theme_name_line_edit.text
 	theme_name_line_edit.text = ""
 	remove_theme_button.disabled = false
 	add_theme_button.disabled = true
+	_update_settings()
 
 
 func _on_RemoveThemeButton_pressed() -> void:
@@ -125,12 +136,22 @@ func _on_RemoveThemeButton_pressed() -> void:
 	
 	if not selected_theme_option_button.has_selectable_items():
 		remove_theme_button.disabled = true
+		theme_settings_container.hide()
+	else:
+		var next_item_idx: int = selected_theme_option_button.get_selectable_item()
+		_selected_theme = selected_theme_option_button.get_item_text(next_item_idx)
+		_update_settings()
 
 
 func _on_SelectedThemeOptionButton_item_selected(idx: int) -> void:
 	_selected_theme = selected_theme_option_button.get_item_text(idx)
-	_update_audio_files()
+	_update_settings()
 
 
 func _on_ThemeNameLineEdit_text_changed(new_text: String) -> void:
 	add_theme_button.disabled = new_text.is_empty()
+
+
+func _on_OverwriteThemeToggle_toggled(is_toggled: bool) -> void:
+	_data[_selected_theme].overwrites_existing_theme = is_toggled
+	data_changed.emit(_data)
