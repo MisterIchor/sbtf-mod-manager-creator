5a882d42a1780fba203b2d1203b67744a74ff9a1
MisterIchor <superluigibrosforever@gmail.com>
2025-05-28 01:40:47 -0400

    Added static class Prompts to allow global access to commonly-used window prompts.

---
+++ src/global/prompts.gd
@@ -0,0 +1,5 @@
+class_name Prompt
+extends Object
+
+static var nwf_path: PackedScene = preload("res://src/prompts/nwf_path/nwf_path.tscn")
+static var invalid_file: PackedScene = preload("res://src/prompts/nwf_path/invalid_file.tscn")
