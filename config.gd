17f61a2fd4831893c932c96be1a637e6cd6b1b84
MisterIchor <superluigibrosforever@gmail.com>
2025-05-30 09:10:33 -0400

    Allowed properties to call save() when changed. save() and load() now account for mods_folder.

--- config.gd
+++ config.gd
@@ -2,9 +2,18 @@ extends Node
 
 const DEFAULT_PATH: String = "user://sbtf_mod_manager.cfg"
 
-var path_to_nwf: String = ""
-var mods_folder: String = ""
-var mod_order: PackedStringArray = []
+var path_to_nwf: String = "":
+	set(value):
+		path_to_nwf = value
+		save()
+var mods_folder: String = "":
+	set(value):
+		mods_folder = value
+		save()
+var mod_order: PackedStringArray = []:
+	set(value):
+		mod_order = value
+		save()
 
 
 
@@ -21,6 +30,7 @@ func save() -> Error:
 		return err_code
 	
 	config.set_value("General", "path_to_nwf", path_to_nwf)
+	config.set_value("General", "mods_folder", mods_folder)
 	
 	for i in mod_order.size():
 		config.set_value("Mod Order", str("mod[", i, "]"), mod_order[i])
@@ -35,8 +45,8 @@ func load() -> Error:
 	if not err_code == OK:
 		return err_code
 	
-	mod_order.clear()
 	path_to_nwf = config.get_value("General", "path_to_nwf")
+	mods_folder = config.get_value("General", "mods_folder")
 	
 	for i in config.get_section_keys("Mod Order"):
 		mod_order.append(config.get_value("Mod Order", i))
