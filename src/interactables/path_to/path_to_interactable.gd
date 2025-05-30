59b40594b62f928b0f4ee2c3f2057e7df28061f3
MisterIchor <superluigibrosforever@gmail.com>
2025-05-30 07:21:16 -0400

    Allowed PathToInteractable to add a "blanket" over the screen to prevent inputs to the main wundow while a file dialog is up.

--- src/interactables/path_to/path_to_interactable.gd
+++ src/interactables/path_to/path_to_interactable.gd
@@ -8,7 +8,7 @@ signal path_set(new_path: String)
 @onready var dir_button: Button = $Button
 @onready var line_edit: LineEdit = $LineEdit
 
-static var _is_in_dialog: bool = false
+var _blanket: ColorRect = null
 
 
 
@@ -24,23 +24,25 @@ func set_text(new_text: String) -> void:
 
 
 func _on_Button_pressed() -> void:
-	if _is_in_dialog:
-		return
-	
 	var file_dialog: FileDialog = FileDialog.new()
 	
+	_blanket = ColorRect.new()
+	_blanket.size = get_window().size
+	_blanket.color = Color(Color.BLACK, 0.2)
+	_blanket.mouse_filter = Control.MOUSE_FILTER_STOP
+	get_tree().current_scene.add_child(_blanket)
 	file_dialog.filters = file_type_filter
 	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
 	file_dialog.access = FileDialog.ACCESS_FILESYSTEM
 	file_dialog.size = Vector2(900, 360)
 	file_dialog.file_selected.connect(_on_FileDialog_file_selected.bind(file_dialog))
+	file_dialog.canceled.connect(_on_FileDialog_file_selected.bind(file_dialog).bind(""))
 	file_dialog.popup_exclusive_centered(owner)
-	_is_in_dialog = true
 
 
 func _on_FileDialog_file_selected(path : String, file_dialog: FileDialog) -> void:
 	set_text(path)
 	file_dialog.hide()
 	file_dialog.queue_free()
-	_is_in_dialog = false
 	path_set.emit(path)
+	_blanket.queue_free()
