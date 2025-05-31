407aacf03c8213645ca7e1d8af03b2b58a2c91ab
MisterIchor <superluigibrosforever@gmail.com>
2025-05-31 18:22:20 -0400

    Creator/Data Change: split the text string from the data file so we can get the line number instead of the cursor position. Erase the changed value from the data dict if value == default value. Accounted for changes in SBTFTool.

--- src/2_creator/data_change.gd
+++ src/2_creator/data_change.gd
@@ -18,39 +18,47 @@ func _ready() -> void:
 
 func _on_DataFilesList_item_selected(idx: int) -> void:
 	var data_file: FileAccess = FileAccess.open(
-		str(SBTFTool.OUTPUT_PATH, "/", data_files_list.get_item_text(idx)),
+		Config.path_to_output.path_join(data_files_list.get_item_text(idx)),
 		FileAccess.READ
 	)
-	
 	if not data_file:
 		return
 	
 	for i in data_property_list.get_children():
 		i.queue_free()
 	
-	while not data_file.eof_reached():
-		var property_string: String = data_file.get_line()
+	var data_file_lines: PackedStringArray = data_file.get_as_text().split("\n")
+	
+	for i in data_file_lines.size():
+		var line: String = data_file_lines[i]
 		
-		if property_string.is_empty():
+		if line.is_empty() or line.begins_with("\r"):
 			continue
 		
 		var new_interactable: TextInteractable = Interactable.instantiate()
 		
-		new_interactable.label_name = property_string.get_slice("=", 0)
-		new_interactable.default_text = property_string.get_slice("=", 1)
-		new_interactable.text_set.connect(_on_TextInteractable_text_set.bind(new_interactable).bind(data_file.get_position()))
+		new_interactable.label_name = line.get_slice("=", 0)
+		new_interactable.default_text = line.get_slice("=", 1)
+		new_interactable.text_set.connect(_on_TextInteractable_text_set.bind(new_interactable).bind(i))
 		data_property_list.add_child(new_interactable)
 
 
 
 func _on_TextInteractable_text_set(new_text: String, line_position: int, interactable: TextInteractable) -> void:
+	var selected_data_file: String = data_files_list.get_item_text(data_files_list.get_selected_items()[0])
+	var changes_dict: Dictionary = _data.get_or_add(selected_data_file, {})
+	var value_name: String = interactable.label_name.trim_suffix(":")
+	
 	if not new_text == interactable.default_text:
-		_data[interactable.label_name.trim_suffix(":")] = {
-			value = new_text,
+		changes_dict[value_name] = {
+			value = new_text.erase(new_text.length() - 1, 2),
 			line_position = line_position
 		}
 	
 	if new_text == interactable.default_text:
-		_data.erase(interactable.label_name.trim_suffix(":"))
+		changes_dict.erase(value_name)
+		
+		if changes_dict.is_empty():
+			_data.erase(selected_data_file)
 	
 	data_changed.emit(_data)
