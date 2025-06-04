d8ca2a3810521b417b1b77761977c989d37d3b34
MisterIchor <superluigibrosforever@gmail.com>
2025-06-04 01:25:46 -0400

    Boot: finished nwf file check process.

--- src/0_boot/boot.gd
+++ src/0_boot/boot.gd
@@ -1,24 +1,47 @@
-extends Window
+extends TextureRect
 
-const SBTFToolExecutationPath: String = "res://sbtftool/sbtftool"
 const NwfPath: PackedScene = preload("res://src/prompts/nwf_path/nwf_path.tscn")
+const InvalidFile: PackedScene = preload("res://src/prompts/nwf_path/invalid_file.tscn")
 
 
 
 func _ready() -> void:
-	var output: Array = []
-	
-	if not Config.load() == OK:
-		var path_window: PopupPanel = NwfPath.instantiate()
-		
-		Config.create()
-		path_window.instantiate().popup_exclusive_centered(self)
-		
+	if not SBTFTool.verify_nwf() == 0:
 		while true:
-			await path_window.popup_hide
+			var nwf_window: AcceptDialog = NwfPath.instantiate()
 			
-			if not OS.execute(ProjectSettings.globalize_path(SBTFToolExecutationPath), ["verify", Config.path_to_nwf], output, true) == 115:
-				break
+			nwf_window.popup_exclusive_centered.call_deferred(self)
+			nwf_window.close_requested.connect(_on_close_requested)
+			await nwf_window.confirmed
+			nwf_window.queue_free()
 			
-			path_window = NwfPath.instantiate().popup_exclusive_centered(self)
+			await get_tree().process_frame
+			if not SBTFTool.verify_nwf() == 0:
+				var invalid_window: AcceptDialog = InvalidFile.instantiate()
+				
+				invalid_window.popup_exclusive_centered.call_deferred(self)
+				await invalid_window.confirmed
+				continue
+			
+			break
 	
+	var cmdline_arguments: PackedStringArray = OS.get_cmdline_user_args()
+	
+	for i in cmdline_arguments:
+		if "--start" in i:
+			var section: String = i.get_slice("=", 1)
+			
+			if section.matchn("manager"):
+				get_tree().change_scene_to_file("res://src/2_manager/manager.tscn")
+				return
+			
+			if section.matchn("creator"):
+				get_tree().change_scene_to_file("res://src/2_creator/create_a_mod.tscn")
+				return
+	
+	get_tree().change_scene_to_file("res://src/1_start/start.tscn")
+
+
+
+func _on_close_requested() -> void: 
+	get_tree().quit()
