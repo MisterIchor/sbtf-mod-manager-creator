541db3424a53b14fc745334848f212dc7723f017
MisterIchor <superluigibrosforever@gmail.com>
2025-06-04 01:30:58 -0400

    SBTFTool: Checks for a backup nwf file before using the default nwf file.

--- src/global/sbtf_tool.gd
+++ src/global/sbtf_tool.gd
@@ -2,8 +2,6 @@ class_name SBTFTool
 extends Object
 
 const EXEC_PATH: String = "res://src/sbtftool/sbtftool"
-
-static var repacked_nwf_path: String = "/home/misterichor/.steam/steam/ubuntu12_32/steamapps/content/app_357330/depot_357331/sbtf_pub.nwf"
 static var _output: Array = []:
 	set(value):
 		_output = value
@@ -25,6 +23,10 @@ static func _get_config_path_to_nwf() -> String:
 	return Config.path_to_nwf
 
 
+static func _get_config_path_to_nwf_backup() -> String:
+	return Config.path_to_nwf.get_base_dir().path_join("sbtf_pub_backup.nwf")
+
+
 static func _get_relative_path_to_schema() -> String:
 	return _get_config_path_to_output().split("/")[-1].path_join("schema.xml")
 
@@ -34,19 +36,26 @@ static func get_global_tool_path() -> String:
 	return str('"', OS.get_executable_path().get_base_dir().path_join(EXEC_PATH.trim_prefix("res://")), '"')
 
 
-
 static func verify_nwf() -> int:
 	return OS.execute(get_global_tool_path(), ["verify", _get_config_path_to_nwf()], _output, true)
 
 
 static func unpack_nwf() -> int:
-	prints(get_global_tool_path(), ["unpack", _get_config_path_to_nwf(), "-o", _get_config_path_to_output()])
+	if FileAccess.open(_get_config_path_to_nwf_backup(), FileAccess.READ):
+		print("Backup nwf file found.")
+		return OS.execute(get_global_tool_path(), ["unpack", _get_config_path_to_nwf_backup(), "-o", _get_config_path_to_output()], _output, true)
+	
+	DirAccess.remove_absolute(_get_config_path_to_output())
+	DirAccess.make_dir_absolute(_get_config_path_to_output())
 	return OS.execute(get_global_tool_path(), ["unpack", _get_config_path_to_nwf(), "-o", _get_config_path_to_output()], _output, true)
 
 
 static func repack_nwf() -> int:
-	return OS.execute(get_global_tool_path(), ["repack", _get_config_path_to_schema(), _get_config_path_to_output(), repacked_nwf_path], _output, true)
+	return OS.execute(get_global_tool_path(), ["repack", _get_config_path_to_schema(), _get_config_path_to_output(), _get_config_path_to_nwf()], _output, true)
 
 
 static func generate_schema() -> int:
+	if FileAccess.open(_get_config_path_to_nwf_backup(), FileAccess.READ):
+		return OS.execute(get_global_tool_path(), ["schema", _get_config_path_to_nwf_backup(), _get_relative_path_to_schema()], _output, true)
+	
 	return OS.execute(get_global_tool_path(), ["schema", _get_config_path_to_nwf(), _get_relative_path_to_schema()], _output, true)
