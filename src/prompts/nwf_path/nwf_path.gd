2052dcbe2d07abbe316a588c1a9bf462cc296dd8
MisterIchor <superluigibrosforever@gmail.com>
2025-06-04 01:35:40 -0400

    NWF Path: Ensured that the file dialog button is disabled properly no matter how file dialog is handled.

--- src/prompts/nwf_path/nwf_path.gd
+++ src/prompts/nwf_path/nwf_path.gd
@@ -1,32 +1,34 @@
-extends Window
+extends AcceptDialog
 
 @onready var path_to_line_edit: LineEdit = $VBoxContainer/PathSelector/PathToLineEdit
 @onready var file_dialog_button: Button = $VBoxContainer/PathSelector/FileDialogButton
-@onready var ok_button: Button = $VBoxContainer/OKButton
 @onready var file_dialog: FileDialog = $FileDialog
 
 
 
 func _ready() -> void:
-	ok_button.pressed.connect(_on_OKButton_pressed)
+	confirmed.connect(_on_handled)
+	canceled.connect(_on_handled)
 	file_dialog_button.pressed.connect(_on_FileDialogButton_pressed)
 	file_dialog.file_selected.connect(_on_FileDialog_file_selected)
+	file_dialog.confirmed.connect(_on_FileDialog_handled)
+	file_dialog.canceled.connect(_on_FileDialog_handled)
 
 
 
-func _on_OKButton_pressed() -> void:
-	if not SBTFTool.verify_nwf() == 0:
-		var prompt: AcceptDialog = Prompt.invalid_file.instantiate()
-		prompt.popup_exclusive_centered(self)
-		return
-	
+func _on_handled() -> void:
 	Config.path_to_nwf = path_to_line_edit.text
-	queue_free()
+
+
+func _on_FileDialog_handled() -> void:
+	file_dialog_button.disabled = false
 
 
 func _on_FileDialogButton_pressed() -> void:
 	file_dialog.popup()
+	file_dialog_button.disabled = true
 
 
 func _on_FileDialog_file_selected(path: String) -> void:
 	path_to_line_edit.text = path
+	file_dialog_button.disabled = false
