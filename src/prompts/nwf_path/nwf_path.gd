4d0e4c9cb4e9fef0504673996fa2f3ea8e6425a3
MisterIchor <superluigibrosforever@gmail.com>
2025-06-05 23:18:26 -0400

    nwf_path.gd: Moved config path setting to _on_FileDialog_file_selected. Removed _on_handled.

--- src/prompts/nwf_path/nwf_path.gd
+++ src/prompts/nwf_path/nwf_path.gd
@@ -7,17 +7,10 @@ extends AcceptDialog
 
 
 func _ready() -> void:
-	confirmed.connect(_on_handled)
-	canceled.connect(_on_handled)
 	file_dialog_button.pressed.connect(_on_FileDialogButton_pressed)
 	file_dialog.file_selected.connect(_on_FileDialog_file_selected)
 	file_dialog.confirmed.connect(_on_FileDialog_handled)
 	file_dialog.canceled.connect(_on_FileDialog_handled)
-
-
-
-func _on_handled() -> void:
-	Config.path_to_nwf = path_to_line_edit.text
 
 
 func _on_FileDialog_handled() -> void:
@@ -31,4 +24,5 @@ func _on_FileDialogButton_pressed() -> void:
 
 func _on_FileDialog_file_selected(path: String) -> void:
 	path_to_line_edit.text = path
+	Config.path_to_nwf = path_to_line_edit.text
 	file_dialog_button.disabled = false
